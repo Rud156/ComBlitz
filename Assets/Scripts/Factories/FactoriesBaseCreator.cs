5426b0f53b31d69a66ef039142558eb428d78cb0
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-09-06 13:11:05 +0530

    Adding Factories Controller

---
+++ Assets/Scripts/Factories/FactoriesBaseCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DeBomb.Factories
+{
+    public abstract class FactoriesBaseCreator : MonoBehaviour
+    {
+        public GameObject unitCreated;
+        public float timeBetweenSpawn;
+
+        [Header("Debug")]
+        public bool spawnOnStart;
+
+        protected Coroutine coroutine;
+
+        private void Start()
+        {
+            if (spawnOnStart)
+                StartSpawn();
+        }
+
+        public void StartSpawn() => coroutine = StartCoroutine(SpawnUnits());
+
+        public void StopSpawn() => StopCoroutine(coroutine);
+
+        public abstract IEnumerator SpawnUnits();
+    }
+}
