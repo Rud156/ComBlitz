926e8cf7c70ac7a79435c740424ab6bd924730b2
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-09-06 10:46:51 +0530

    Added Bullet Damage

--- Assets/Scripts/Player/Shooter/BulletDamageManager.cs
+++ Assets/Scripts/Player/Shooter/BulletDamageManager.cs
@@ -4,6 +4,8 @@ namespace DeBomb.Player.Shooter
 {
     class BulletDamageManager : MonoBehaviour
     {
-
+        public float bulletDamage = 20f;
+        
+        private void OnTriggerEnter(Collider other) => Destroy(gameObject);
     }
 }
