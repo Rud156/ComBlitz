e783b75366ffbfa2971ddb05a5ac4ab99e3b8fad
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-09-06 22:17:55 +0530

    Updated Enemy Prefabs and Added Enemy Controller

---
+++ Assets/Scripts/ConstantData/TagManager.cs
@@ -0,0 +1,10 @@
+﻿namespace DeBomb.ConstantData
+{
+    public static class TagManager
+    {
+        public const string Player = "Player";
+        public const string Enemy = "Enemy";
+        public const string Base = "Base";
+        public const string Platform = "ConstructionPlatforms";
+    }
+}
---
+++ Assets/Scripts/Enemy/Base/EnemyMovementBase.cs
@@ -0,0 +1,51 @@
+﻿using DeBomb.ConstantData;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DeBomb.Enemy
+{
+    public abstract class EnemyMovementBase : MonoBehaviour
+    {
+        public float maxPLayerTargetDistance;
+
+        private NavMeshAgent enemyAgent;
+
+        private Transform playerTransform;
+        private Transform baseTransform;
+        private Transform[] constructionPlatforms;
+
+        private void Start()
+        {
+            enemyAgent = GetComponent<NavMeshAgent>();
+
+            playerTransform = GameObject.FindGameObjectWithTag(TagManager.Player).transform;
+            baseTransform = GameObject.FindGameObjectWithTag(TagManager.Base).transform;
+            constructionPlatforms = GameObject.FindGameObjectsWithTag(TagManager.Platform)
+                .Select(_ => _.transform).ToArray();
+        }
+
+        private void Update()
+        {
+            CheckPlayerDistance();
+        }
+
+        private void CheckPlayerDistance()
+        {
+            if (Vector3.Distance(playerTransform.position, transform.position) < maxPLayerTargetDistance)
+                enemyAgent.SetDestination(playerTransform.position);
+            else
+            {
+                if(constructionPlatforms.Length > 0)
+                {
+
+                }
+            }
+
+        }
+
+        public void AttackObject()
+        {
+        }
+    }
+}
