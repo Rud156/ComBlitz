7dc1503dc26d3f8067764be8edd06ad8cd3d557f
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-09-14 01:26:48 +0530

    Added Button Audio

---
+++ Assets/Scripts/UI/ButtonHoverAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ComBlitz.UI
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class ButtonHoverAudio : MonoBehaviour
+    {
+        private AudioSource audioSource;
+
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start() => audioSource = GetComponent<AudioSource>();
+
+        public void OnPointerEnter() => audioSource.Play();
+    }
+}
